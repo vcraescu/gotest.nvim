eb0f7b523073ba67bbda50704268439dc734a831
Viorel Craescu <viorel@craescu.com>
2025-09-10 09:25:32 +0300

    add prefix to messages

--- lua/gotest/notify.lua
+++ lua/gotest/notify.lua
@@ -1,30 +1,32 @@
 local M = {}
 
+local msg_prefix = "[gotest] "
+
 ---@param msg string
 function M.warn(msg)
   vim.schedule(function()
-    vim.api.nvim_echo({ { msg, "DiagnosticWarn" } }, true, {})
+    vim.api.nvim_echo({ { msg_prefix .. msg, "DiagnosticWarn" } }, true, {})
   end)
 end
 
 ---@param msg string
 function M.error(msg)
   vim.schedule(function()
-    vim.api.nvim_echo({ { msg, "DiagnosticError" } }, true, {})
+    vim.api.nvim_echo({ { msg_prefix .. msg, "DiagnosticError" } }, true, {})
   end)
 end
 
 ---@param msg string
 function M.success(msg)
   vim.schedule(function()
-    vim.api.nvim_echo({ { msg, "DiagnosticHint" } }, true, {})
+    vim.api.nvim_echo({ { msg_prefix .. msg, "DiagnosticHint" } }, true, {})
   end)
 end
 
 ---@param msg string
 function M.info(msg)
   vim.schedule(function()
-    vim.api.nvim_echo({ { msg, "DiagnosticInfo" } }, true, {})
+    vim.api.nvim_echo({ { msg_prefix .. msg, "DiagnosticInfo" } }, true, {})
   end)
 end
 
