27a819991b9bb0d334c6484cf46970a4ef4cac95
Viorel Craescu <viorel@craescu.com>
2026-03-14 15:56:18 +0200

    have no clue

--- lua/gotest/ts.lua
+++ lua/gotest/ts.lua
@@ -57,9 +57,7 @@ function M.get_current_test_func_name(bufnr)
 
   parser:parse()
 
-  local winid = vim.fn.bufwinid(bufnr)
-  local cursor = vim.api.nvim_win_get_cursor(winid ~= -1 and winid or 0)
-  local node = vim.treesitter.get_node({ bufnr = bufnr, pos = { cursor[1] - 1, cursor[2] } })
+  local node = vim.treesitter.get_node({ bufnr = bufnr })
   if not node then
     return
   end
